e2a3025d92a9ae2084288adde06eb016ec909a2d
Cácio Lucas <cclucas060901@gmail.com>
2024-11-14 11:06:03 -0300

    Remove backend directory to add as submodule

--- backend/core/service.py
+++
@@ -1,7 +0,0 @@
-from core.repository import BaseRepository
-from core.singleton import SingletonMeta
-
-
-class BaseService(metaclass=SingletonMeta):
-    def __init__(self, repository: BaseRepository):
-        self.repository = repository
